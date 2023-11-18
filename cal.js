29747f6856d910c8d8cade3eb9f99c399870eca9
MUSKAN1804 <125110236+MUSKAN1804@users.noreply.github.com>
2023-11-18 21:49:17 +0530

    Update cal.js

--- cal.js
+++ cal.js
@@ -1,2 +1,20 @@
-let currentValue = '';
-document.querySelector('#display').value = currentValue;
+let input = document.getElementById('display');
+let buttons = document.querySelectorAll('.button');
+let string = '';
+let arr = Array.from('button');
+arr.forEach('button')=>{
+button.addEventListener('click',(e){
+  if(e.taget.innerHTML == '='){
+    string = eval(string);
+    input.value = string;
+  }
+  else if(e.target.innerHTML == 'C'){
+  string = '';
+    input.value = string;
+  }
+  else{
+    string += e.taget.innerHTML;
+    input.value = string;
+  }
+})}
+                  
